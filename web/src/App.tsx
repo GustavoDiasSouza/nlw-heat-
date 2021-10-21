7c43e0a44da389af933ec8ad71d8afb21175afb0
Gustavo <gustavosouza0@yahoo.com>
2021-10-21 18:25:14 -0300

    salvo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import styles from './App.module.scss';
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
 
 export function App() {
   return (
     <main className={styles.contentWrapper}>
-      <h1>Avião!!!</h1>
+      <MessageList/>
+
+      <LoginBox/>
 
     </main>
   )
